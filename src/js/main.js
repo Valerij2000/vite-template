4a2cba28647b45b57cb1a90bdbf4d498e41c0b1c
Валерий <valery.shumkov@mail.ru>
2025-06-11 10:01:45 +0300

    init

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import '../scss/style.scss';
+
+// import MicroModal from 'micromodal';
+// MicroModal.init({
+//   awaitOpenAnimation: true,
+//   awaitCloseAnimation: true,
+//   disableScroll: true,
+//   disableOverlayClose: true
+// });
